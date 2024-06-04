77be6f6335e8157490426b9bc97265a4326a03b9
Gaúcho <joao_c_caetano@estudante.sesisenai.org.br>
2024-06-04 09:11:43 -0300

    Organizando os exercícios.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+//ExercÍcio 01
+const dobrar = (a, b) => a*2 + "," + b*2;
+
+// Exercício 02
 const ordenarPalavra = ["Maionese caseira", "Arroz", "Batata-palha", "Filé", "Feijoada"];
-ordenarPalavra.sort((a, b) => a.length - b.length);
+ordenarPalavra.sort((a, b) => a.length - b.length); // Usando .sort para ordenar do menor para o maior.
 console.log(ordenarPalavra);
 
+// Exercício 03
 const numeroPar = [5, 2, 78, 100, 17];
-const resultado = numeroPar.filter((numeroPar) => numeroPar % 2 == 0);
+const resultado = numeroPar.filter((numeroPar) => numeroPar % 2 == 0); // O filter serve para "filtrarmos" somente o que queremos que seja mostrado.
 console.log(resultado);
